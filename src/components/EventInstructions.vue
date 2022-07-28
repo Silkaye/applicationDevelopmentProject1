db50f7ffe98d8690c8f8e9050b681f9df97788d1
Silkaye <91874661+Silkaye@users.noreply.github.com>
2022-07-28 16:27:45 +1000

    Commit

---
+++ src/components/EventInstructions.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="eventinstructions">
+    <h1>Long Jump</h1>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,11 +1,34 @@
 <template>
   <main class="eventsView">
     <section class="events">
-      <EventCard 
-        v-for="event in events"
-        :key="event.title"
-        :event="event"
-      />
+      <div class="tileholder">
+        <div class="tile is-ancestor">
+          <div class="tile is-4 is-vertical is-parent">
+            <div class="tile is-child box">
+              <EventCard :event="events[0]" />
+            </div>
+            <div class="tile is-child box">
+              <EventCard :event="events[1]" />
+            </div>
+          </div>
+          <div class="tile is-4 is-vertical is-parent">
+            <div class="tile is-child box">
+              <EventCard :event="events[0]" />
+            </div>
+            <div class="tile is-child box">
+              <EventCard :event="events[1]" />
+            </div>
+          </div>
+          <div class="tile is-4 is-vertical is-parent">
+            <div class="tile is-child box">
+              <EventCard :event="events[0]" />
+            </div>
+            <div class="tile is-child box">
+              <EventCard :event="events[1]" />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
@@ -23,24 +46,45 @@ import EventCard from '../components/EventCard.vue';
         events: [
           {
             title: 'Long Jump',
-            src: require('../assets/long-jump-w.png'),
+            src: require('../assets/long-jump.png'),
             name: 'longjump',
           },
           {
             title: 'Sprint',
-            src: require('../assets/long-jump-w.png'),
+            src: require('../assets/long-jump.png'),
             name: 'settings',
           },
           {
             title: 'Shotput',
-            src: require('../assets/long-jump-w.png')
+            src: require('../assets/long-jump.png')
           }
         ],
+        eventSelected: false
       }
     },
   }
 </script>
 
-<style scoped>
+<style scoped src="bulma/css/bulma.min.css"></style>
 
+<style scoped>
+  .tileholder {
+    max-width: 1000px;
+  }
+
+  .events {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .box {
+    background-color: var(--dark);
+    transition: 0.2s ease-in-out;
+  }
+
+  .box:hover {
+    background-color: var(--dark-alt);
+    transition: 0.2s ease-in-out;
+  }
 </style>
